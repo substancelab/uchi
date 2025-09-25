8009dd069a596831015cea74520ea8e6a29fd33f
Jakob Skjerning <jakob@mentalized.net>
2025-09-25 23:29:35 +0200

    Extract superclass

--- app/components/uchi/ui/pagination/next_link.rb
+++ app/components/uchi/ui/pagination/next_link.rb
@@ -3,21 +3,7 @@
 module Uchi
   module Ui
     class Pagination
-      class NextLink < ViewComponent::Base
-        attr_reader :paginator
-
-        # @param paginator [Pagy::Pagy] The Pagy object
-        def initialize(paginator:)
-          super()
-          @paginator = paginator
-        end
-
-        # Returns the URL for a given page. page can be a number, of :first,
-        # :last, :previous, :next, :current.
-        def page_url(page)
-          paginator.page_url(page)
-        end
-
+      class NextLink < Item
         # Returns true if there is a next page
         def next?
           paginator.next.present?
