af183c5a00c289b73d93b095b3b070364f2432cf
Jakob Skjerning <jakob@mentalized.net>
2025-10-27 10:28:20 +0100

    Show flash messages in toasts

--- app/helpers/uchi/application_helper.rb
+++ app/helpers/uchi/application_helper.rb
@@ -1,4 +1,17 @@
 module Uchi
   module ApplicationHelper
+    # Maps Rails flash types to Flowbite Toast styles
+    def flash_style(flash_type)
+      case flash_type.to_sym
+      when :success
+        :success
+      when :alert, :error
+        :danger
+      when :warning
+        :warning
+      else
+        :default
+      end
+    end
   end
 end
