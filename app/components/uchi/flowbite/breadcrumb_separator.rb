ecf7b0d869d1a2e96be91f8a2c553a8d0e43a068
Jakob Skjerning <jakob@mentalized.net>
2025-11-30 18:53:02 +0100

    Use correct Flowbite 4 breadcrumb separator SVG

--- app/components/uchi/flowbite/breadcrumb_separator.rb
+++ app/components/uchi/flowbite/breadcrumb_separator.rb
@@ -15,14 +15,16 @@ module Uchi::Flowbite
         "aria-hidden": "true",
         xmlns: "http://www.w3.org/2000/svg",
         fill: "none",
-        viewBox: "0 0 6 10"
+        height: 24,
+        viewBox: "0 0 24 24",
+        width: 24
       ) do
         tag.path(
           stroke: "currentColor",
           "stroke-linecap": "round",
           "stroke-linejoin": "round",
           "stroke-width": "2",
-          d: "m1 9 4-4-4-4"
+          d: "m9 5 7 7-7 7"
         )
       end
     end
