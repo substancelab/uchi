ee3ab5aa6e846dc7930af117d046dd4bff8b63d3
Jakob Skjerning <jakob@mentalized.net>
2025-12-31 12:43:15 +0100

    Extract default mount path into a method

--- lib/uchi/routes.rb
+++ lib/uchi/routes.rb
@@ -7,8 +7,17 @@ module Uchi
     #  Rails.application.routes.draw do
     #    Uchi.routes.mount(self)
     #  end
-    def mount(host_routes)
-      host_routes.mount(Uchi::Engine, at: "/uchi")
+    def mount(host_routes, at: default_at)
+      host_routes.mount(
+        Uchi::Engine,
+        at: at
+      )
+    end
+
+    private
+
+    def default_at
+      "uchi"
     end
   end
 end
