2c2c465587e45d7313eb556edb844b86bf521a27
Jakob Skjerning <jakob@mentalized.net>
2026-01-26 16:12:32 +0100

    Guard against silly input value

--- lib/uchi/routes.rb
+++ lib/uchi/routes.rb
@@ -13,7 +13,7 @@ module Uchi
     #
     # @param at [Symbol] The path segment where Uchi should be mounted.
     def mount(host_routes, at: default_at)
-      @mount_at = at.to_sym
+      @mount_at = (at || default_at).to_sym
       host_routes.mount(
         Uchi::Engine,
         as: mount_as,
