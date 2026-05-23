095821c11f07c3d1e88d56eafd9ba7776eadcf16
Jakob Skjerning <jakob@mentalized.net>
2026-05-23 21:16:46 +0200

    Extract common superclass for field components

--- app/components/uchi/field/base.rb
+++ app/components/uchi/field/base.rb
@@ -3,24 +3,7 @@
 module Uchi
   class Field
     class Base < Field
-      # Uchi::Field::Base::Edit components render fields in the edit view.
-      class Edit < ViewComponent::Base
-        attr_reader :field, :form, :record, :repository, :label, :hint
-
-        def initialize(field:, form:, repository:, label: nil, hint: nil)
-          super()
-
-          @field = field
-          @form = form
-          @label = label
-          @hint = hint
-          @record = form.object
-          @repository = repository
-        end
-      end
-
-      # Uchi::Field::Base::Show components render fields in the show view.
-      class Show < ViewComponent::Base
+      class Component < ViewComponent::Base
         attr_reader :field, :record, :repository
 
         def initialize(field:, record:, repository:)
@@ -32,10 +15,20 @@ module Uchi
         end
       end
 
-      # Uchi::Field::Base::Index components render fields in the index view.
-      class Index < ViewComponent::Base
-        attr_reader :field, :record, :repository
+      # Uchi::Field::Base::Edit components render fields in the edit view.
+      class Edit < Component
+        attr_reader :form, :label, :hint
 
+        def initialize(field:, form:, repository:, label: nil, hint: nil)
+          super(field:, record: form.object, repository:)
+          @form = form
+          @label = label
+          @hint = hint
+        end
+      end
+
+      # Uchi::Field::Base::Index components render fields in the index view.
+      class Index < Component
         class << self
           # Returns the CSS classes to apply to the td or th of the table where
           # this field is rendered.
@@ -43,14 +36,10 @@ module Uchi
             []
           end
         end
+      end
 
-        def initialize(field:, record:, repository:)
-          super()
-
-          @field = field
-          @record = record
-          @repository = repository
-        end
+      # Uchi::Field::Base::Show components render fields in the show view.
+      class Show < Component
       end
     end
   end
