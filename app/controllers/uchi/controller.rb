892d1b0518b213ea37a1a22da70134d4e04863ba
Jakob Skjerning <jakob@mentalized.net>
2026-09-07 14:51:12 +0200

    Clarify controllers

--- app/controllers/uchi/controller.rb
+++ app/controllers/uchi/controller.rb
@@ -5,6 +5,13 @@
 # All Uchi controllers should inherit from Uchi::ApplicationController. This
 # controller is here to provide us a set of defaults that can be overridden in
 # consumer code.
+#
+# - `Uchi::Controller` is an internal base controller for the Uchi engine, which
+#   shouldn't be seen or used directly by consumer applications.
+# - `Uchi::ApplicationController` is intended to be the base controller for
+#   consumer applications, allowing them to customize behavior without touching
+#   the internal `Uchi::Controller`.
+
 module Uchi
   class Controller < ActionController::Base
     layout "uchi/application"
