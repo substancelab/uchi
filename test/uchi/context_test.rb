ff1034abfdad44bd1a053593070fca62202a3916
Jakob Skjerning <jakob@mentalized.net>
2026-09-08 17:05:57 +0200

    Store Uchi::Views in Context#view

--- test/uchi/context_test.rb
+++ test/uchi/context_test.rb
@@ -18,6 +18,36 @@ class UchiContextTest < ActiveSupport::TestCase
     assert_equal :index, @context.view
   end
 
+  test "#view= accepts nil" do
+    @context.view = nil
+
+    assert_nil @context.view
+  end
+
+  test "#view= accepts String" do
+    @context.view = "new"
+
+    assert_equal Uchi::View.new(:new), @context.view
+  end
+
+  test "#view= raises ArgumentError for false" do
+    assert_raises(ArgumentError) do
+      @context.view = false
+    end
+  end
+
+  test "#view= raises ArgumentError for an unknown view name" do
+    assert_raises(ArgumentError) do
+      @context.view = :bogus
+    end
+  end
+
+  test "#view= raises ArgumentError for a value that can't resolve to a view" do
+    assert_raises(ArgumentError) do
+      @context.view = Object.new
+    end
+  end
+
   test "attributes default to nil" do
     assert_nil @context.repository
     assert_nil @context.user
