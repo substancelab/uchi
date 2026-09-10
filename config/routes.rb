2a482912f86b4ac4313af9d905db57d17415fc02
Jakob Skjerning <jakob@mentalized.net>
2026-09-10 17:09:58 +0200

    Simplify routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Uchi::Engine.routes.draw do
     namespace :has_many do
       resources :associated_records, only: [:index]
     end
-    resources :search, only: [:index], path: "search", as: "search"
+    resources :search, only: [:index], path: "search"
     namespace :search do
       resources :results, only: [:index]
     end
