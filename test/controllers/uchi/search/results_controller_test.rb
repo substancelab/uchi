63dc7da05fb8dc7bee3cd0553ee406d5f1c657be
Jakob Skjerning <jakob@mentalized.net>
2026-09-10 17:01:06 +0200

    Use URL helpers
    The test had been changed to compute the expected edit link via
    
        edit_uchi_book_path(@hobbit.id)
    
    after the
    
        get uchi.search_results_path(...)
    
    call.
    
    Because Uchi::Engine is mounted, processing a request routed into the
    engine sets @request.script_name to "/uchi" for the rest of the test
    session.
    
    Any subsequent call to a main-app route helper (like
    edit_uchi_book_path) then picks up that leftover script_name and
    prepends it again, producing /uchi/uchi/books/1/edit instead of
    /uchi/books/1/edit.

--- test/controllers/uchi/search/results_controller_test.rb
+++ test/controllers/uchi/search/results_controller_test.rb
@@ -14,10 +14,19 @@ module Uchi
       end
 
       test "GET index links to matching records" do
+        # Because Uchi::Engine is mounted with isolate_namespace, processing a
+        # request routed into the engine sets `@request.script_name` to "/uchi"
+        # for the rest of the test session. Any subsequent call to a main-app
+        # route helper (like edit_uchi_book_path) then picks up that leftover
+        # script_name and prepends it again, producing /uchi/uchi/books/1/edit
+        # instead of /uchi/books/1/edit.
+        path_to_hobbit = uchi_book_path(@hobbit.id)
+        path_to_silmarillion = uchi_book_path(@silmarillion.id)
+
         get uchi.search_results_path(repository: "books", query: "Hobbit")
 
-        assert_select "a[data-turbo-frame='_top'][href=?]", "/uchi/books/#{@hobbit.id}/edit"
-        assert_select "a[href=?]", "/uchi/books/#{@silmarillion.id}/edit", count: 0
+        assert_select "a[data-turbo-frame='_top'][href=?]", path_to_hobbit
+        assert_select "a[href=?]", path_to_silmarillion, count: 0
       end
 
       test "GET index shows a heading with the repository name when there are matches" do
