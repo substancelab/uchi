bbdff753c3e31c0b6c87307786d63fd9b9fd37ae
Jakob Skjerning <jakob@mentalized.net>
2026-09-17 19:32:11 +0200

    Prevent mutation of the constants

--- lib/uchi/view/constants.rb
+++ lib/uchi/view/constants.rb
@@ -5,15 +5,15 @@ require "uchi/view"
 module Uchi
   class View
     # Define some commonly used views as constants for convenience.
-    EDIT = new(:edit)
-    INDEX = new(:index)
-    NEW = new(:new)
-    SHOW = new(:show)
+    EDIT = new(:edit).freeze
+    INDEX = new(:index).freeze
+    NEW = new(:new).freeze
+    SHOW = new(:show).freeze
 
     # Shorthand for all views.
-    ALL = [EDIT, INDEX, NEW, SHOW]
+    ALL = [EDIT, INDEX, NEW, SHOW].freeze
 
     # Shorthand for views with a form
-    FORM = [EDIT, NEW]
+    FORM = [EDIT, NEW].freeze
   end
 end
