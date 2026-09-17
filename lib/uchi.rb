8cc2eea87ce5ecb13e2562e79ecf0ca9f5170cd1
Jakob Skjerning <jakob@mentalized.net>
2026-09-17 19:20:17 +0200

    Tweak load order

--- lib/uchi.rb
+++ lib/uchi.rb
@@ -16,7 +16,7 @@ require "uchi/plugins"
 require "uchi/repository"
 require "uchi/routes"
 require "uchi/sort_order"
-require "uchi/view/constants"
+require "uchi/view"
 require "uchi/repository/translate"
 
 module Uchi
